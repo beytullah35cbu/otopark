e73f728df3ab5be06af58bfc590dfe30be3c6508
beytullah <114831003+beytullah35cbu@users.noreply.github.com>
2023-10-27 08:20:08 +0300

    Update Program.cs

--- otopark/otopark/Program.cs
+++ otopark/otopark/Program.cs
@@ -1,90 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-/*
- 
-
-Otomobil için 2 saat: 5tl sonraki her saat 1 tl, 10 saatten sonrası günlük sabit ücret…
-Kamyon için 2 saat 8 tl, sonraki her saat 2 tl, 8 saatten sonra günlük sabit ücret
-İş makinası 2 saat 12 tl sonraki her saat 3 tl, 8 saatten sonra günlük sabit
-
-
-*/
-namespace Otopark_Hesaplama
+namespace deneme1111
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-         Console.WriteLine("Araç türünü seçin (1: Otomobil, 2: Kamyon, 3: İş Makinası):");
-        int aracTuru = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Kaç saat kiralamak istersiniz?");
-        int kiralamaSuresi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Araç türünü seçin: (1: Otomobil, 2: Kamyon, 3: İş Makinası)");
+            int aracTuru = int.Parse(Console.ReadLine());
 
-        double toplamUcret = 0;
+            Console.Write("Araç kaç saat boyunca kullanılacak? ");
+            double kullanmaSuresi = Convert.ToDouble(Console.ReadLine());
 
-        switch (aracTuru)
-        {
-            case 1: // Otomobil
-                    //Otomobil için 2 saat: 5tl sonraki her saat 1 tl, 10 saatten sonrası günlük sabit ücret
-                    if (kiralamaSuresi <= 2)
-                {
-                    toplamUcret = 5;
-                }
-                else if (kiralamaSuresi <= 10)
-                {
-                    toplamUcret = 5 + (kiralamaSuresi - 2);
-                }
-                else
-                {
-                    toplamUcret = 5 + 8 + (kiralamaSuresi - 10);
-                }
-                break;
+            double ucret;
+            switch (aracTuru)
+            {
+                case 1: // Otomobil için 2 saat: 5tl sonraki her saat 1 tl, 10 saatten sonrası günlük sabit ücret…
+                    if (kullanmaSuresi <= 2)
+                    {
+                        ucret = 5;
+                    }
+                    else if (kullanmaSuresi <= 10)
+                    {
+                        ucret = 5 + (kullanmaSuresi - 2) * 1;
+                    }
+                    else
+                    {
+                        ucret = 5 + 8; // 10 saatten sonra sabit ücret
+                    }
+                    break;
 
-            case 2: // Kamyon
-                    //Kamyon için 2 saat 8 tl, sonraki her saat 2 tl, 8 saatten sonra günlük sabit ücret
-                    if (kiralamaSuresi <= 2)
-                {
-                    toplamUcret = 8;
-                }
-                else if (kiralamaSuresi <= 8)
-                {
-                    toplamUcret = 8 + 2 * (kiralamaSuresi - 2);
-                }
-                else
-                {
-                    toplamUcret = 8 + 2 * 6 + (kiralamaSuresi - 8);
-                }
-                break;
+                case 2: // Kamyon için 2 saat 8 tl, sonraki her saat 2 tl, 8 saatten sonra günlük sabit ücret
+                    if (kullanmaSuresi <= 2)
+                    {
+                        ucret = 8;
+                    }
+                    else if (kullanmaSuresi <= 8)
+                    {
+                        ucret = 8 + (kullanmaSuresi - 2) * 2;
+                    }
+                    else
+                    {
+                        ucret = 8 + 12; // 8 saatten sonra sabit ücret
+                    }
+                    break;
 
-            case 3: // İş Makinası
-                    //İş makinası 2 saat 12 tl sonraki her saat 3 tl, 8 saatten sonra günlük sabit
-                    if (kiralamaSuresi <= 2)
-                {
-                    toplamUcret = 12;
-                }
-                else if (kiralamaSuresi <= 8)
-                {
-                    toplamUcret = 12 + 3 * (kiralamaSuresi - 2);
-                }
-                else
-                {
-                    toplamUcret = 12 + 3 * 6 + (kiralamaSuresi - 8);
-                }
-                break;
+                case 3: // İş makinası 2 saat 12 tl sonraki her saat 3 tl, 8 saatten sonra günlük sabit
+                    if (kullanmaSuresi <= 2)
+                    {
+                        ucret = 12;
+                    }
+                    else if (kullanmaSuresi <= 8)
+                    {
+                        ucret = 12 + (kullanmaSuresi - 2) * 3;
+                    }
+                    else
+                    {
+                        ucret = 12 + 18 ; // 8 saatten sonra sabit ücret
+                    }
+                    break;
 
-            default:
-                Console.WriteLine("Geçersiz araç türü seçildi.");
-                return;
-        }
+                default:
+                    Console.WriteLine("Geçersiz araç türü!");
+                    return;
+            }
 
-        Console.WriteLine($"Toplam kira ücreti: {toplamUcret} TL");
-            Console.ReadKey();
+            Console.WriteLine($"Seçilen araç türü için saatlik ücret: {ucret} TL");
+            Console.ReadKey(); // Programın kapanmasını engellemek için
         }
     }
 }
