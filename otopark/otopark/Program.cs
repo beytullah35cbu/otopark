dfaff6b3f3ea1e7c2faa59888b3ec834252387eb
beytullah <114831003+beytullah35cbu@users.noreply.github.com>
2023-10-28 12:00:23 +0300

    Update Program.cs

--- otopark/otopark/Program.cs
+++ otopark/otopark/Program.cs
@@ -1,73 +1,96 @@
 using System;
 
-namespace otoparkodev
+namespace otopark
 {
+    // Otomobil için 2 saat: 5tl sonraki her saat 1 tl, 10 saatten sonrası günlük sabit ücret…
+    // Kamyon için 2 saat 8 tl, sonraki her saat 2 tl, 8 saatten sonra günlük sabit ücret
+    // İş makinası 2 saat 12 tl sonraki her saat 3 tl, 8 saatten sonra günlük sabit
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("araba turu gır otomobil kamyon iş makınesı (1/2/3)");
+            int aracturu = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Araç türünü seçin: (1: Otomobil, 2: Kamyon, 3: İş Makinası)");
-            int aracTuru = int.Parse(Console.ReadLine());
 
-            Console.Write("Araç kaç saat boyunca kullanılacak? ");
-            double kullanmaSuresi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ne kadar kalcn");
+            double kullanımsuresı = Convert.ToDouble(Console.ReadLine());
+
+
 
             double ucret;
-            switch (aracTuru)
+
+            switch (aracturu)
             {
-                case 1: // Otomobil için 2 saat: 5tl sonraki her saat 1 tl, 10 saatten sonrası günlük sabit ücret…
-                    if (kullanmaSuresi <= 2)
+                case 1:
+                    if (kullanımsuresı <=2)
                     {
                         ucret = 5;
+
                     }
-                    else if (kullanmaSuresi <= 10)
+                    else if (kullanımsuresı<=10)
                     {
-                        ucret = 5 + (kullanmaSuresi - 2) * 1;
+                        ucret = 5 + (kullanımsuresı - 2) * 1; 
+
                     }
                     else
                     {
-                        ucret = 5 + 8; // 10 saatten sonra sabit ücret
+                        ucret = 5 + 8; //10saat sonrası sabıt ucret dıyor yanı 2 saat 5se 8saat 8tl
                     }
+
                     break;
 
-                case 2: // Kamyon için 2 saat 8 tl, sonraki her saat 2 tl, 8 saatten sonra günlük sabit ücret
-                    if (kullanmaSuresi <= 2)
+                case 2:
+                    if (kullanımsuresı <= 2)
                     {
                         ucret = 8;
+
                     }
-                    else if (kullanmaSuresi <= 8)
+                    else if (kullanımsuresı <= 10)
                     {
-                        ucret = 8 + (kullanmaSuresi - 2) * 2;
+                        ucret = 8 + (kullanımsuresı - 2) * 2;
+
                     }
                     else
-                    {
-                        ucret = 8 + 12; // 8 saatten sonra sabit ücret
+                    {                   //dıkkat sabıt ucret saat bası 2tl
+                        ucret = 8 + 12; //8saat sonrası sabıt ucret dıyor yanı 2 saat 8se 6saat 12 yapar 
                     }
+
                     break;
 
-                case 3: // İş makinası 2 saat 12 tl sonraki her saat 3 tl, 8 saatten sonra günlük sabit
-                    if (kullanmaSuresi <= 2)
+
+                case 3:
+                    if (kullanımsuresı <= 2)
                     {
                         ucret = 12;
+
                     }
-                    else if (kullanmaSuresi <= 8)
+                    else if (kullanımsuresı <= 10)
                     {
-                        ucret = 12 + (kullanmaSuresi - 2) * 3;
-                    }
+                        ucret = 12 + (kullanımsuresı - 2) * 3;
+
+                    }   
                     else
-                    {
-                        ucret = 12 + 18 ; // 8 saatten sonra sabit ücret
+                    {               //dıkkat saat bası 3tl
+                        ucret = 12 + 18; //hemen matematık *_*
+                                          //2 saat 12 ise 8 saat soruyor 6 * 3 = 18 olur
                     }
+
                     break;
 
                 default:
-                    Console.WriteLine("Geçersiz araç türü!");
+                    Console.Write("hatalı deger gırdın");
+
                     return;
+
+
             }
 
-            Console.WriteLine($"Seçilen araç türü için saatlik ücret: {ucret} TL");
-            Console.ReadKey(); // Programın kapanmasını engellemek için
+
+
+            Console.Write("ucretiniz  " + ucret);
+
+            Console.ReadKey();           
         }
     }
 }
