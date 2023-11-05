dfe084fb45cad331f0ded2126bf4312e4185b387
beytullah <114831003+beytullah35cbu@users.noreply.github.com>
2023-11-05 21:06:43 +0300

    Update Program.cs

--- otopark/otopark/Program.cs
+++ otopark/otopark/Program.cs
@@ -1,23 +1,24 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace otopark
 {
     class Program
     {
+
+       /* Otomobil için 2 saat: 5tl sonraki her saat 1 tl, 10 saatten sonrası günlük sabit ücret…
+Kamyon için 2 saat 8 tl, sonraki her saat 2 tl, 8 saatten sonra günlük sabit ücret
+İş makinası 2 saat 12 tl sonraki her saat 3 tl, 8 saatten sonra günlük sabit
+       */
         static void Main(string[] args)
         {
-
-
-            // Otomobil için 2 saat: 5tl sonraki her saat 1 tl, 10 saatten sonrası günlük sabit ücret…
-            // Kamyon için 2 saat 8 tl, sonraki her saat 2 tl, 8 saatten sonra günlük sabit ücret
-            // İş makinası 2 saat 12 tl sonraki her saat 3 tl, 8 saatten sonra günlük sabit
             Console.WriteLine("Araç türünü girin (Otomobil: 1, Kamyon: 2, İş Makinesi: 3):");
             int aracTuru = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Kaldığınız süreyi saat cinsinden girin:");
             double kullanimSuresi = Convert.ToDouble(Console.ReadLine());
 
-            double ucret = 0;
+            double ucret;
 
             switch (aracTuru)
             {
@@ -32,10 +33,10 @@ namespace otopark
                     }
                     else
                     {
-                        int gun = (int)kullanimSuresi / 24; // tam gün
-                        double kalanSaat = kullanimSuresi % 24; // 24 saatten fazla saati hesapla
-                        ucret = 5 + 8 * gun + kalanSaat; //gunluk ucret 2 saat 5tl 8saat 8 tl kısacası sabıt ucret
-                    }
+                        int tamGun = (int)kullanimSuresi / 24; // Tam gün
+                        double kalanSaat = kullanimSuresi % 24; // 24 saat mi
+                        ucret = (tamGun * 13) + 5 + (kalanSaat - 2) * 1; // 2saat 5 tl 8saat 8tl topla sabıt ucret 13
+                    }               //gun varsa sabıt ucretle carp  dogal olarak dıger gunun 2 saat ucretını ekle
                     break;
 
                 case 2:
@@ -49,10 +50,10 @@ namespace otopark
                     }
                     else
                     {
-                        int gun = (int)kullanimSuresi / 24; 
-                        double kalanSaat = kullanimSuresi % 24; //dikkat 8saat sonra sabıt ucret ve saat bası 2tl
-                        ucret = 8 + 12 * gun + kalanSaat; // 2 saati 8tl  6saati 12tl 
-                    }
+                        int tamGun = (int)kullanimSuresi / 24;
+                        double kalanSaat = kullanimSuresi % 24;
+                        ucret = (tamGun * 20) + 8 + (kalanSaat - 2) * 2; // 2saat ıcın 8 6 saat ıcın 12
+                    } //20 sabıt ucret
                     break;
 
                 case 3:
@@ -66,10 +67,10 @@ namespace otopark
                     }
                     else
                     {
-                        int gun = (int)kullanimSuresi / 24; 
-                        double kalanSaat = kullanimSuresi % 24; //8saat sonra sabıt ucret 3tl
-                        ucret = 12 + 18 * gun + kalanSaat; //2 saat 12 tl 6 saat 18
-                    }
+                        int tamGun = (int)kullanimSuresi / 24;
+                        double kalanSaat = kullanimSuresi % 24;
+                        ucret = (tamGun * 30) + 12 + (kalanSaat - 2) * 3; // 2 saat ıcın 12 6saat ıcın 18
+                    }           //sabit ucret 30
                     break;
 
                 default:
